907cf416e5dc9d635e482fdcc7f2e28bb0065a1e
Antony Bailey <support@antonybailey.net>
2017-03-26 22:30:11 +0100

    Apparently AppVeyor wants TestCategory too.

--- src/ParseNodes.Tests/UnitTest1.cs
+++ src/ParseNodes.Tests/UnitTest1.cs
@@ -5,13 +5,13 @@ namespace ParseNodes.Test
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
+        [TestMethod, TestCategory("Get Exit Nodes")]
         public void GetExitNodeList()
         {
             Assert.IsNotNull(new Parser().GetExitNodes());
         }
 
-        [TestMethod]
+        [TestMethod, TestCategory("Serialise Exit Nodes")]
         public void SerialiseExitNodes()
         {
             Assert.IsNotNull(new Serialiser().SerialiseExitNodes(new Parser().GetExitNodes()));
